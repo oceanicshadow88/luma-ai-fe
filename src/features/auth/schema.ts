c14a855e3690efd85f2335fe46ef72a7035b8c17
shengfengpeng <terencep429@gmail.com>
2025-05-21 21:51:20 +1000

    refactor: add one more comment to explain the schema file of using Zod

--- src/features/auth/schema.ts
+++ src/features/auth/schema.ts
@@ -1,4 +1,5 @@
 import * as z from 'zod'; // TypeScript-first schema validation library with static type inference. Reference: https://github.com/colinhacks/zod
+                          // Zod is designed to centralize validation logic in schema files. Referenc: https://zod.dev/?id=writing-schemas; https://react-hook-form.com/get-started#SchemaValidation
 import { emailSchema, verificationCodeSchema, passwordSchema } from '@schema/validation';
 
 export const resetPasswordSchema = z

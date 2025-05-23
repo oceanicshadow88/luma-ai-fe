6f2c16c0a92e1eec1dbdae6f1e74475bd6323fd6
shengfengpeng <terencep429@gmail.com>
2025-05-23 15:06:49 +1000

    refactor: improve semantization of login/page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,38 +1,38 @@
 import { Link } from "react-router-dom";
-import title from '@assets/logo.svg';
+import logo from '@assets/logo.svg';
 import background from '@assets/decorative_graphic.png';
 import { LoginForm } from "@features/auth/components/LoginForm";
 
 const LoginPage = () => {
     return (
         <div className="flex flex-col lg:flex-row w-full min-h-screen">
-            <div className="w-full lg:w-3/5 pl-30 p-8 flex flex-col justify-center items-start bg-white">
-                <div className="mb-8 flex flex-row items-center gap-2">
-                    <img src={title} alt="title" className="h-8" />
-                </div>
+            <main className="w-full lg:w-3/5 pl-30 p-8 flex flex-col justify-center items-start bg-white">
+                <header className="mb-8 flex flex-row items-center gap-2">
+                    <img src={logo} alt="Luma AI Logo" className="h-8" />
+                </header>
 
-                <div className="mb-4 text-left">
-                    <h1 className="text-1xl font-semibold text-gray-800">Log in to Luma AI Enterprise Version</h1>
-                </div>
+                <section className="mb-4 text-left">
+                    <h1 className="text-xl font-semibold text-gray-800">Log in to Luma AI Enterprise Version</h1>
+                </section>
 
-                <div className="w-full lg:w-5/6 max-w-md" >
+                <section className="w-full lg:w-5/6 max-w-md">
                     <LoginForm />
-                </div>
+                </section>
 
-                <div className="p-6 ml-7 items-center text-sm">
-                    <span className="text-gray-600">Forgot Password?</span>
-                    <Link to="/auth/reset-password" className="ml-1 text-blue-600 font-medium hover:text-blue-800 hover:underline">
+                <section className="p-6 ml-7 text-sm">
+                    <p className="text-gray-600 inline">Forgot Password?</p>
+                    <Link to="/auth/reset-password" className="ml-1 text-blue-600 font-medium hover:text-blue-900 hover:underline">
                         RESET YOUR PASSWORD
                     </Link>
-                </div>
-            </div>
+                </section>
+            </main>
 
-            <div className="hidden lg:block w-full lg:w-2/5">
+            <aside className="hidden lg:block w-full lg:w-2/5" aria-hidden="true">
                 <img
                     src={background}
                     alt="Decorative pattern background"
                 />
-            </div>
+            </aside>
         </div>
     );
 };

034498d55a94a76496b454918eca8a4877da5579
shengfengpeng <terencep429@gmail.com>
2025-06-06 14:51:51 +1000

    refactor:update api base url

--- src/services/api/apiClient.ts
+++ src/services/api/apiClient.ts
@@ -2,7 +2,7 @@ import { ApiError } from '@custom-types/ApiError';
 import axios, { AxiosError, AxiosHeaders, type InternalAxiosRequestConfig } from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://api.example.com';
-const API_VERSION = '/v1';
+const API_VERSION = '/api/v1';
 
 export const apiClient = axios.create({
   baseURL: `${BASE_URL}${API_VERSION}`,

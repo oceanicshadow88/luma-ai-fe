8e63b8d8523b99013ac4c6c88087ef00e618f1c2
kitman <kitman200220022002@gmail.com>
2025-06-16 23:08:07 +1000

    la-25 revert vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,12 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
-    port: 5173, 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/v1'),
+      },
+    },
   },
 });

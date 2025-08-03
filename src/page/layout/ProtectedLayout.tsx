95b81fdbd35ef1da35ac639e481bf96295536d89
Cristine <cristineypj@gmail.com>
2025-08-03 12:34:55 +1000

    fix: la-117 jump dashboard page after signup successfully

--- src/page/layout/ProtectedLayout.tsx
+++ src/page/layout/ProtectedLayout.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Outlet } from 'react-router-dom';
 import LoginPage from '@app/auth/login/page';
 import NotFoundPage from '@components/layout/NotFoundPage';
 import { isMainDomain } from '@utils/domainUtils';
+import { decodeJwt } from '../../utils/jwtUtils';
+import { hasExpiry } from '../../utils/dataUtils';
+import { Outlet } from 'react-router-dom';
 
-const isAuth = (): boolean => {
-  return true;
+const isAccessTokenValid = (): boolean => {
+  const token = localStorage.getItem('accessToken') ?? '';
+  const decodedPayload: any = token ? decodeJwt(token) : null;
+  
+  return !!token && !!decodedPayload && !hasExpiry(decodedPayload.exp);
 };
 
 const ProtectedLayout = () => {
   if (isMainDomain()) {
     return <NotFoundPage />;
   }
-  if (isAuth()) {
+
+  if (!isAccessTokenValid()) {
     return <LoginPage />;
   }
 
